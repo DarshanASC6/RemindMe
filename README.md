12c09aa933be7186d3353e9f9fb2b55478f1a32e
Darshan Singh <darshan.singh.tech@gmail.com>
2020-05-03 14:45:24 -0400

    Updated documentation and script
    Multiple requests can be made, just not used as of right now.

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ A python script that will remind me to do certain things every so often. Basical
 
 # How does it work?
 Good question. I'm not completely sure myself.
+
+# How do I use it?
+1. Enter a list of things to be reminded about, seperated by a comma and a space
+    Ex: Remind me to: eat, sleep
+
+2. Enter how long until you're reminded to do them
+    Ex: How many minutes until I remind you? 5
+    (5 minutes until the reminder)
+
+3. Wait to be reminded
--- RemindMe.py
+++ RemindMe.py
@@ -1,1 +1,43 @@
-print("Hello")
+import time
+import ctypes  # An included library with Python install.   
+
+tasks = input("Remind me to: ")
+# Asks the user what they want to be reminded to do later on
+
+x = tasks.split(", ")
+# variable that splits up the string into multiple inputs
+
+duration = float(input("How many minutes until I remind you? "))
+# Asks the user how frequently they want to be reminded to do their task
+
+if duration < 1:
+    print("Reminding you to", x[0], "in", duration, "minutes")
+# Basic grammer stuff because I'm picky. States both the reminder and how long until the reminder.
+
+elif duration > 1:
+    print("Reminding you to", x[0], "in", duration, "minutes")
+# Basic grammer stuff because I'm picky. States both the reminder and how long until the reminder.
+
+else:
+    print("Reminding you to", x[0], "in", duration, "minutes")
+# Basic grammer stuff because I'm picky. States both the reminder and how long until the reminder.
+
+
+duration = duration * 60
+# This converts duration from seconds to minutes
+
+counter = 0
+# Variable that counts the number of reminders printed
+
+while 1 == 1:
+    time.sleep(duration)
+    # Waits for the user input amount of time
+    
+    ctypes.windll.user32.MessageBoxW(0, x[0], "RemindMe.py Reminder", 0)
+    # Creates a popup message that won't go away until the user acknowledges it
+    
+    counter = counter + 1
+    # Increases the number of reminders
+    
+    print("Reminders given to", x[0], ": ", counter)
+    # Shows how many reminders have been given
